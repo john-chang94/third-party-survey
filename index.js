d6989b7c1916d236c32665870b33241cc2285454
john-chang94 <39566708+john-chang94@users.noreply.github.com>
2019-07-25 18:56:25 -0700

    cookie update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const passport = require('passport');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./services/passport');
 
@@ -13,10 +14,17 @@ mongoose.connect(keys.MONGO_URI, { useNewUrlParser: true })
 
 const app = express();
 
-// app.use(passport.initialize());
+app.use(cookieSession({
+    // 30 days, 24 hours, 60 mins, 60 seconds, 1000 milliseconds == 30 days // the total is based off milliseconds
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
+// require('./routes/authRoutes')(app);
 const authRouter = require('./routes/authRoutes');
-app.use('/', authRouter);
+app.use(authRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,4 +8,13 @@ router.get('/auth/google', passport.authenticate('google', {
 
 router.get('/auth/google/callback', passport.authenticate('google'));
 
+router.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+})
+
+router.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+})
+
 module.exports = router;
